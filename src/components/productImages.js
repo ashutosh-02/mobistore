6cce499246efa5c07ca66d5c66ee5f5ba851d361
ashutosh-02 <ashutosh2002k@gmail.com>
2024-08-24 00:18:26 +0530

    Updated App.js to use productData.js and productImages.js for product management

---
+++ src/components/productImages.js
@@ -0,0 +1,34 @@
+// productImages.js
+import product1Img from '../assets/images/product1.jpg';
+import product2Img from '../assets/images/product2.jpg';
+import product3Img from '../assets/images/product3.jpg';
+import product4Img from '../assets/images/product4.jpg';
+import product5Img from '../assets/images/product5.jpg';
+import product6Img from '../assets/images/product6.jpg';
+import product7Img from '../assets/images/product7.jpg';
+import product8Img from '../assets/images/product8.jpg';
+import product10Img from '../assets/images/product10.jpg';
+import product11Img from '../assets/images/product11.jpg';
+import product12Img from '../assets/images/product12.jpg';
+import product13Img from '../assets/images/product13.jpg';
+import product14Img from '../assets/images/product14.jpg';
+import product15Img from '../assets/images/product15.jpg';
+import product16Img from '../assets/images/product16.jpg';
+
+export {
+    product1Img,
+    product2Img,
+    product3Img,
+    product4Img,
+    product5Img,
+    product6Img,
+    product7Img,
+    product8Img,
+    product10Img,
+    product11Img,
+    product12Img,
+    product13Img,
+    product14Img,
+    product15Img,
+    product16Img,
+};
